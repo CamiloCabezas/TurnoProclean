cfa141bee218249fbae51d951a24d28ea1caa320
Juan Camilo Cabezas Galvis <camilocabezasg@gmail.com>
2025-11-04 20:07:31 -0500

    Pruebas de filtros y renderizacion

---
+++ TurnoProFornt/TurnoPro/src/components/filters/filters.jsx
@@ -0,0 +1,31 @@
+    import React, { useState } from "react";
+    import DatePicker from "react-datepicker";
+    import "./filters.css"; 
+    import "react-datepicker/dist/react-datepicker.css";
+
+const DateFilter = () => {
+    
+    const [dateInicio, setDateInicio] = useState("");
+    console.log(dateInicio);
+    const [dateFin, setDateFin] = useState("");
+    console.log(dateFin);
+    
+
+    return (
+        <div className="date-filter-container">
+        <h2>Selecciona un día o un rango</h2>
+            <label>Desde: </label>
+            <input 
+            type="date"
+            onChange={(e) => setDateInicio(e.target.value)}/>
+            <label>Hasta: </label>
+            <input 
+            type="date"
+            onChange={(e) => setDateFin(e.target.value)}/>
+
+
+        </div>
+    );
+    };
+
+    export default DateFilter;
--- TurnoProFornt/TurnoPro/src/routes/menu/menu.jsx
+++ TurnoProFornt/TurnoPro/src/routes/menu/menu.jsx
@@ -1,9 +1,14 @@
 import './menu.css'
 import { get_empleados_empresa } from '../../endpoints/api'
 import { useState, useEffect } from 'react'
+import DateFilter from '../../components/filters/filters'
+import "react-datepicker/dist/react-datepicker.css";
 
 const Menu = () => {
     const [empleados, setEmpleados] = useState([])
+    // Prueba
+    const [dates, setDates] = useState([])
+    // 
 
   useEffect(() => {
     const fetchEmpleados = async () => {
@@ -19,23 +24,44 @@ const Menu = () => {
     fetchEmpleados();
   }, []);
 
-    return (
-        <div className="menu">
-            <div className="container-menu">
-                <h1 className="title">Personal De Operacion</h1>
 
-                <ul>
-                    {empleados.map((empleado, index) => (
-                        <li key={index}>{empleado.nombre}</li>
-                    ))} 
-                </ul>
 
-            </div>
-            
-            
+  // Esto estara mal pero es para pruebas -----------------
+  useEffect(() => {
+    const start = new Date("2025-11-13");
+    const end = new Date("2025-11-20");
+    const listDates = [];
+    let current = new Date(start)
+    while(current <= end){
+      listDates.push(new Date(current));
+      current.setDate(current.getDate() + 1);
+    }
+    setDates(listDates)
+    console.log(listDates);
+    
+  },[])
+// --------------------------------------------------------
 
-        </div>
-    )
+
+     return (
+    <div className="menu">
+      <DateFilter />
+      <div className="container-menu">
+        <h1 className="title">Personal De Operación</h1>
+
+        {dates.map((date, index) => (
+          <div key={index} className="fecha-section">
+            <h4>{date.toLocaleDateString("es-CO")}</h4>
+            <ul>
+              {empleados.map((empleado, i) => (
+                <li key={i}>{empleado.nombre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Menu
